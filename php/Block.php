d86ec6e536c0c15bf68e7337fd2a5019e7bb5d87
Evan Agee <evanagee@gmail.com>
2021-08-17 12:36:06 -0400

    Some performance improvements

--- php/Block.php
+++ php/Block.php
@@ -62,29 +62,20 @@ class Block {
 	 * @return string The markup of the block.
 	 */
 	public function render_callback( $attributes, $content, $block ) {
-		$post_types = get_post_types( [ 'public' => true ] );
-		$class_name = $attributes['className'];
+		$post_types = get_post_types( array( 'public' => true ) );
+		$class_name = esc_attr__( $attributes['className'] );
 		ob_start();
-
 		?>
 		<div class="<?php echo $class_name; ?>">
 			<h2>Post Counts</h2>
 			<?php
 			foreach ( $post_types as $post_type_slug ) :
 				$post_type_object = get_post_type_object( $post_type_slug );
-				$post_count = count(
-					get_posts(
-						[
-							'post_type' => $post_type_slug,
-							'posts_per_page' => -1,
-						]
-					)
-				);
-
-				?>
-				<p><?php echo 'There are ' . $post_count . ' ' . $post_type_object->labels->name . '.'; ?></p>
+				$post_count = wp_count_posts( $post_type_slug );
+			?>
+				<p><?php echo 'There are ' . $post_count->publish . ' ' . $post_type_object->labels->name . '.'; ?></p>
 			<?php endforeach; ?>
-			<p><?php echo 'The current post ID is ' . $_GET['post_id'] . '.'; ?></p>
+			<p><?php echo 'The current post ID is ' . get_the_ID() . '.'; ?></p>
 		</div>
 		<?php
 
